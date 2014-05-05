d8f7ae00f2ae7a75f2b297449bbbe38b0bc141ba
daniel <xiaohutu136@163.com>
2014-05-05 17:41:36 +0800

    update

--- conf/config-dev.js
+++ conf/config-dev.js
@@ -29,7 +29,7 @@ MysqlDB.password = '';
 MysqlDB.database = '';
 
 /* ==== 认证服务，主服务使用Memcached共享Session信息，这里通过去Memcached取得Session信息来判断是否已经登录 === */
-Auth.required = true; // 是否需要认证，如果设置为false，则以下配置都可忽略
+Auth.required = false; // 是否需要认证，如果设置为false，则以下配置都可忽略
 Auth.servers = ['192.168.86.88:11211']; // 值的所有格式参考https://github.com/3rd-Eden/node-memcached的Server locations
 Auth.serverOptions = {}; // 可用配置参考https://github.com/3rd-Eden/node-memcached的Options
 Auth.apiUrls = ['.*']; // 需要登录认证才能调用的API URL，可用正则表达式
